6a90173c3c0f9fa108088134b8208050baef6461
Your Name <you@example.com>
2021-12-06 23:51:43 +0400

    add new file js

---
+++ jspromt/skript.js
@@ -0,0 +1,15 @@
+let age=prompt("Inserte your age");
+
+if(age<18){
+    alert( "hm, come back with dad");
+}
+if(age>18 && age<21){
+    alert("nearly, but not yet bro");
+}
+if(age>21){
+    alert("welcom to the club")
+}
+
+let a=5;
+a += +prompt("Inserte number");
+alert(a);
